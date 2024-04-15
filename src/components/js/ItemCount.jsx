881ff35c8aa4b52aa5d8153eba1d5c50300607d2
Paulo-Estalise <phestalise@gmail.com>
2024-04-15 15:15:25 -0300

    AULA 9 SINCRONIZAR COUNTER

---
+++ src/components/js/ItemCount.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function ItemCount({ stock, initial, onAdd }) {
+    const [count, setCount] = useState(initial);
+
+    const handleIncrement = () => {
+        if (count < stock) {
+            setCount(prevCount => prevCount + 1);
+        }
+    };
+
+    const handleDecrement = () => {
+        if (count > 1) {
+            setCount(prevCount => prevCount - 1);
+        }
+    };
+
+    const handleAddToCart = () => {
+        onAdd(count);
+    };
+
+    return (
+        <div className="item">
+            <span>{count}</span>
+            <button onClick={handleDecrement}>-</button>
+            <button onClick={handleAddToCart}>Adicionar ao carrinho</button>
+            <button onClick={handleIncrement}>+</button>
+        </div>
+    );
+}
+
+// Define default props
+ItemCount.defaultProps = {
+    onAdd: () => {
+        console.warn('onAdd function is not provided');
+    },
+    initial: 1,
+    stock: 10 // Default stock value, you can adjust this as needed
+};
+
+export default ItemCount;
